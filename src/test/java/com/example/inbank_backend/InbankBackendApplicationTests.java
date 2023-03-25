a758776304399b94d88e302db8dd15965529008e
krsobo <krsobo@ttu.ee>
2023-03-25 13:58:09 +0200

    Initial commit

---
+++ src/test/java/com/example/inbank_backend/InbankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inbank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
